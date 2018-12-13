6fee0585fac0484bd5eaaac097d3f6ec70481a71
MrChickenBacon <34237142+MrChickenBacon@users.noreply.github.com>
2018-12-13 21:11:11 +0100

    Code cleaned and optimized. Try catch added.

--- VoiceRecBlank/Program.cs
+++ VoiceRecBlank/Program.cs
@@ -1,24 +1,62 @@
-﻿using System;
+﻿//ChickenBacon Assistant Blank
+
+//BEFORE YOU BEGIN:
+//Add reference "System.Speech" manually
+
+using System;
 using System.Media;
 using System.Speech.Recognition;
 using System.Speech.Synthesis;
 using System.Threading;
 
 namespace VoiceRecBlank
-{ //ChickenBacon Assistant
+{
     class Program
     {
         //Default windows user path example C:\Users\Chris
         public static string Path = $"{Environment.GetFolderPath(Environment.SpecialFolder.UserProfile)}";
 
+        //Test counter, start value 0
         public static int Test { get; set; }
 
-        //Add reference "System.Speech" manually
+        //Speech property
         private static readonly SpeechSynthesizer synthesizer = new SpeechSynthesizer();
 
         static void Main(string[] args)
         {
+            //Optional
+            //synthesizer.SelectVoice("Voice Name");  <= example "Microsoft David". If not, => Use default system voice.
+
+            SpeechRecoEngine();
+            Thread.Sleep(1000);
+            Console.WriteLine("Waiting for voice input.");
+            Console.ReadKey();
+        }
+
+        private static void SpeechRecoEngine()
+        {
             SpeechRecognitionEngine recEngine = new SpeechRecognitionEngine();
+            var gBuilder = CommandsGrammarBuilder();
+            Grammar grammar = new Grammar(gBuilder);
+            recEngine.LoadGrammarAsync(grammar);
+            try
+            {
+                recEngine.SetInputToDefaultAudioDevice();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                Console.WriteLine();
+                Console.WriteLine("Please plug in a microphone.");
+                Console.ReadLine();
+                throw;
+            }
+            recEngine.SpeechRecognized += recEngine_SpeechRecognized;
+            recEngine.RecognizeAsync(RecognizeMode.Multiple);
+        }
+
+        private static GrammarBuilder CommandsGrammarBuilder()
+        {
             Choices commands = new Choices();
 
             //Add voicecommand. Split with comma.
@@ -26,20 +64,9 @@ namespace VoiceRecBlank
             {
                 "test", "number count test", "hello computer"
             });
-
             GrammarBuilder gBuilder = new GrammarBuilder();
             gBuilder.Append(commands);
-            Grammar grammar = new Grammar(gBuilder);
-            recEngine.LoadGrammarAsync(grammar);
-
-            //Sets default input device to be used.
-            recEngine.SetInputToDefaultAudioDevice();
-            recEngine.SpeechRecognized += recEngine_SpeechRecognized;
-            recEngine.RecognizeAsync(RecognizeMode.Multiple);
-
-            Thread.Sleep(1000);
-            Console.WriteLine("Waiting for voice input.");
-            Console.ReadKey();
+            return gBuilder;
         }
 
         private static void recEngine_SpeechRecognized(object sender, SpeechRecognizedEventArgs e)
@@ -48,23 +75,25 @@ namespace VoiceRecBlank
             switch (e.Result.Text)
             {
                 case "test":
-                    SoundPlayer player = new SoundPlayer($@"C:\Windows\media\tada.wav");
-                    player.Play();
+                    using (SoundPlayer player = new SoundPlayer($@"C:\Windows\media\tada.wav"))
+                    {
+                        player.Play();
+                    }
                     Test++;
                     Console.WriteLine("Did you hear the tada sound?");
                     break;
                 case "number count test":
-                    synthesizer.SpeakAsync("Test has been said " + Test + " times." );
+                    synthesizer.SpeakAsync("Test has been said " + Test + " times.");
                     Console.WriteLine($"{Test}");
                     break;
                 case "hello computer":
                     synthesizer.SpeakAsync("Hello world");
                     Console.WriteLine("Hello world");
                     break;
-                //case "add your own command":
-                //    Action;
-                //    Console.WriteLine("Test");
-                //    break;
+                    //case "add your own command":
+                    //    Action;
+                    //    Console.WriteLine("Test");
+                    //    break;
             }
         }
     }
